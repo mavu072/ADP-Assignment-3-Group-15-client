d0532d97fb1834069c3a871ea399f672a27c8215
Avuyile Mgxotshwa <mavu072@gmail.com>
2021-10-20 21:03:04 +0200

    refactored

--- src/main/java/za/ac/cput/ADP3LabBookingSystem/View/App.java
+++ src/main/java/za/ac/cput/ADP3LabBookingSystem/View/App.java
@@ -14,61 +14,11 @@ import java.awt.event.ActionListener;
 import java.awt.font.TextAttribute;
 import java.io.IOException;
 import java.sql.*;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Map;
 
 public class App {
-
-    private static OkHttpClient createAuthenticatedClient(final String username, final String password) {
-
-        // create client with authentication info
-        OkHttpClient httpClient = new OkHttpClient.Builder().authenticator(new Authenticator() {
-
-            public Request authenticate(Route route, Response response) throws IOException {
-                String credential = Credentials.basic(username, password);
-                return response
-                        .request()
-                        .newBuilder()
-                        .header("Authorization", credential)
-                        .build();
-            }
-        }).build();
-
-        return httpClient;
-    }
-
-    private static OkHttpClient client = createAuthenticatedClient("admin", "admin1234");
-
-    private static String run(String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().toString();
-        }
-    }
-
-    public static void getAll() {
-        final String URL = "http://localhost:8090/computerlab/getall";
-        try {
-            String responseBody = run(URL);
-            System.out.println(responseBody);
-            JSONArray computerLabs = new JSONArray(responseBody);
-
-            for (int i = 0; i < computerLabs.length(); i++) {
-                JSONObject computerLab = computerLabs.getJSONObject(i);
-
-                Gson g = new Gson();
-                ComputerLab c =g.fromJson(computerLab.toString(), ComputerLab.class);
-                System.out.println(c.toString());
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
     /************************************************** GUI CODE ****************************************************/
     //                                                              //
@@ -1253,8 +1203,6 @@ public class App {
     /***************************************************************************************************************************************************/
 
     public static void main(String[] args) {
-
-        getAll();
 
         /* IF THIS IS THE FIRST EXEC. THEN UNCOMMENT AND RUN THE FOLLOWING */
 
